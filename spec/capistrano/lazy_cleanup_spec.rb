585d34fbb50fed3024530a7acac89e2e6220c66b
Takumasa Ochi <aeroastro007@gmail.com>
2018-09-15 11:29:12 +0900

    Remove unnecessary test

--- spec/capistrano/lazy_cleanup_spec.rb
+++ spec/capistrano/lazy_cleanup_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Capistrano::LazyCleanup do
   it "has a version number" do
     expect(Capistrano::LazyCleanup::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
